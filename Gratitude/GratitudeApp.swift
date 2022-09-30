f2ae985a88234db8a9f512c29bb6411b3c921d8e
sukidhar <sukidhar@gmail.com>
2022-09-30 14:29:59 +0530

    Initial Commit

---
+++ Gratitude/GratitudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratitudeApp.swift
+//  Gratitude
+//
+//  Created by sukidhar on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GratitudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
