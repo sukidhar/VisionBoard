d4f311130eec958c11641029a92b3ed138aceafa
sukidhar <sukidhar@gmail.com>
2022-10-01 16:19:29 +0530

    added Section file to track list

--- Gratitude/Models/Section.swift
+++ Gratitude/Models/Section.swift
@@ -5,35 +5,10 @@
 //  Created by sukidhar on 01/10/22.
 //
 
-import Foundation
 import RealmSwift
 
-class SectionObject : Object{
+class Section : Object, ObjectKeyIdentifiable{
     @Persisted var title: String
-    @Persisted var images: List<VBImageObject>
+    @Persisted var images: List<VBImage>
 }
 
-struct Section {
-    let title: String
-    let images : [VBImage]
-    
-    init(title: String, images: [VBImage]) {
-        self.title = title
-        self.images = images
-    }
-    
-    init(_ sectionObject: SectionObject) {
-        self.title = sectionObject.title
-        self.images = sectionObject.images.map { .init($0) }
-    }
-    
-    func getObject() -> SectionObject{
-        let object = SectionObject()
-        object.title = self.title
-        object.images = .init()
-        self.images.forEach {
-            object.images.append($0.getObject())
-        }
-        return object
-    }
-}
